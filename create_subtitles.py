071992a1f3206bffbc4d0316e56a699b83f9e46d
Jacob Quant <jacobq@gmail.com>
2016-02-11 02:37:51 -0600

    WIP: Adding support for animating an entire sequence w/ fades

--- create_subtitles.py
+++ create_subtitles.py
@@ -6,81 +6,155 @@ def plugin_main(output_folder, text_file, frame_width, frame_height,
                 text_x, text_y,
                 text_width, text_height,
                 font, font_size_px, font_color,
-                save_png, save_xcf, show_images,
+                save_png, show_images,
                 add_outline, outline_color, add_drop_shadow,
-                generate_one_image_per_record, frame_rate,
-                fade_time_sec
+                generate_one_image_per_record,
+                frame_rate,
+                fade_time
                 ):
+    # TODO: Don't assume TSV data is sorted
     # FIXME: Make OS-agnostic, if possible/practical
     directory_separator = "\\"  # Windows
     #directory_separator = "/"  # POSIX
+    
+    
+    def generate_subtitle_image():
+        # Note: if you use RGBA_IMAGE here it doesn't output any warnings, but it gets interpreted as GRAY
+        # Due to confusing the GimpImageBaseType and GimpImageType enum types
+        img = gimp.Image(frame_width, frame_height, RGB) 
+
+        # Create the transparent background "Frame" layer and add it to the image
+        background_layer = gimp.Layer(img, "Frame", frame_width, frame_height, RGBA_IMAGE, 100, NORMAL_MODE)
+        gimp.pdb.gimp_image_insert_layer(img, background_layer, None, -1)
+
+
+        # Create the text layer
+        text_layer = gimp.pdb.gimp_text_fontname(img, None, float(text_x), float(text_y), text, -1, True, font_size_px, PIXELS, font)
+        gimp.pdb.gimp_text_layer_resize(text_layer, text_width, text_height)
+        gimp.pdb.gimp_text_layer_set_justification(text_layer, TEXT_JUSTIFY_CENTER)
+        gimp.pdb.gimp_text_layer_set_color(text_layer, font_color)
+        #gimp.pdb.gimp_image_insert_layer(img, text_layer, None, 1) # Seems to get added automatically so this isn't needed
+
+        # Create outline in layer behind text
+        outline_layer = gimp.Layer(img, "Outline", frame_width, frame_height, RGBA_IMAGE, 100, NORMAL_MODE)
+        gimp.pdb.gimp_image_insert_layer(img, outline_layer, None, 1)
+        
+        # Create path/vectors from text and stroke it on outline layer
+        if (add_outline):
+            vectors = gimp.pdb.gimp_vectors_new_from_text_layer(img, text_layer)
+            gimp.pdb.gimp_image_insert_vectors(img, vectors, None, -1)
+            # Save color before stroking then restore it back afterward
+            prev_fg_color = gimp.get_foreground()
+            gimp.set_foreground(outline_color)
+            gimp.pdb.gimp_edit_stroke_vectors(outline_layer, vectors)
+            gimp.set_foreground(prev_fg_color)
+        
+        # Run "Drop Shadow" plugin/script
+        if (add_drop_shadow):
+            gimp.pdb.script_fu_drop_shadow(img, text_layer, 5, 5, 10, (0,0,0), 100, False)
+
+        return img
+    
+    def save_frame(img):
+        filename_without_extension = "subtitle_" + str(current_frame).zfill(len(str(number_of_labels)))
+        if (save_png):
+            png_file = filename_without_extension + ".png"
+            gimp.pdb.file_png_save_defaults(img, img.active_layer, output_folder + directory_separator + png_file, png_file)
+            
+    # TODO: Support other easing functions besides linear
+    def ease(in_min, in_current, in_max, out_start, out_end, type="linear"):
+        progress = (in_current - in_min) / (in_max - in_min)
+        if (out_start > out_end):
+            progress = 1 - progress
+            temp = out_start
+            out_start = out_end
+            out_end = temp
+        result = progress * (out_end - out_start) + out_start
+        if (result > out_end):
+            gimp.message(" ".join([str(in_min), str(in_current), str(in_max), str(out_start), str(out_end)]));
+            result = out_end
+        return float(result)
 
     with open(text_file) as f:
-        line_number = 1
-        lines = f.readlines()
-        number_of_lines = len(lines)
-        for line in lines:
-            # Parse line: file should be tab-delimited: (start, stop, text)
-            [start, stop, text] = line.split("\t")
+        current_frame = 1
+        current_label = 1
+        labels = f.readlines()
+        number_of_labels = len(labels)
+        for label in labels:
+            # Parse label row/line/record: file should be tab-delimited: (start, stop, text)
+            [start, stop, text] = label.split("\t")
+            start = float(start)
+            stop = float(stop)
             text = text.strip();
-        
-            # limit iterations for debugging
-            if (line_number > 2):
-                break
             
-            # Generate subtitle image
-            img = create_subtitle(text, frame_width, frame_height, text_x, text_y, text_width, text_hegith, font, font_size_px, font_color);
+            # Calculate keyframes for this label/record/cue
+            start_frame = int(start * frame_rate)
+            fade_in_end_frame = int((start + fade_time) * frame_rate)
+            fade_out_start_frame = max(fade_in_end_frame, int((stop - fade_time) * frame_rate))
+            end_frame = int(stop * frame_rate)
 
-            # Show the image
+            # debug
+            if (current_frame == start_frame and current_frame < 1000):
+                gimp.message(
+                      "start: " + str(start) + "\n"
+                    + "stop: " + str(stop) + "\n"
+                    + "text: " + text + "\n"
+                    + "start_frame: " + start_frame + "\n"
+                    + "fade_in_end_frame: " + fade_in_end_frame + "\n"
+                    + "fade_out_start_frame: " + fade_out_start_frame + "\n"
+                    + "end_frame: " + end_frame + "\n"
+                    )
+            
+            # Generate the subtitle image
+            img = generate_subtitle_image()
+            merged_layer = gimp.pdb.gimp_image_merge_visible_layers(img, EXPAND_AS_NECESSARY)
+
             if (show_images):
+                gimp.pdb.gimp_image_clean_all(img)
                 gimp.Display(img)
                 gimp.displays_flush()
+            
+            # Start as faded-out
+            gimp.pdb.gimp_layer_set_opacity(merged_layer, 0)
 
-            filename_without_extension = "subtitle_" + str(line_number).zfill(len(str(number_of_lines)))
-            if (save_xcf):
-                xcf_file = filename_without_extension + ".xcf"
-                gimp.pdb.gimp_xcf_save(0, img, img.active_layer, output_folder + directory_separator + xcf_file, xcf_file)
-            if (save_png):
-                merged_layer = gimp.pdb.gimp_image_merge_visible_layers(img, 0)
-                png_file = filename_without_extension + ".png"
-                gimp.pdb.file_png_save_defaults(img, merged_layer, output_folder + directory_separator + png_file, png_file)
-
+                        
+            # Seek first point where we fade this in
+            while (current_frame < start_frame):
+                # use last frame
+                save_frame(img)
+                current_frame += 1
+            
+            # Fade-in
+            while (current_frame < fade_in_end_frame):
+                gimp.pdb.gimp_layer_set_opacity(merged_layer, ease(start_frame, current_frame, fade_in_end_frame, 0, 100))
+                save_frame(img)
+                current_frame += 1
+            
+            # Now we're faded-in
+            gimp.pdb.gimp_layer_set_opacity(merged_layer, 100)
+            
+            # Seek fade-out start
+            while (current_frame < fade_out_start_frame):
+                save_frame(img)
+                current_frame += 1
+                
+            # Fade-out
+            while (current_frame < end_frame):
+                gimp.pdb.gimp_layer_set_opacity(merged_layer, ease(fade_out_start_frame, current_frame, end_frame, 0, 100))
+                save_frame(img)
+                current_frame += 1
+            
+            # Now we've faded out
+            gimp.pdb.gimp_layer_set_opacity(merged_layer, 0)
             if (not(show_images)):
                 gimp.pdb.gimp_image_delete(img)
 
-            line_number += 1
-
+            current_label += 1
+            # limit iterations for debugging
+            if (current_label > 1):
+                break
                 
-def create_subtitle(text, frame_width, frame_height, text_x, text_y, text_width, text_height, font, font_size_px, font_color):
-    # Note: if you use RGBA_IMAGE here it doesn't output any warnings, but it gets interpreted as GRAY
-    # Due to confusing the GimpImageBaseType and GimpImageType enum types
-    img = gimp.Image(width, height, RGB) 
-
-    # Create the transparent background "Frame" layer and add it to the image
-    background_layer = gimp.Layer(img, "Frame", frame_width, frame_height, RGBA_IMAGE, 100, NORMAL_MODE)
-    gimp.pdb.gimp_image_insert_layer(img, background_layer, None, -1)
-
-
-    # Create the text layer
-    text_layer = gimp.pdb.gimp_text_fontname(img, None, float(x), float(y), text, -1, True, font_size_px, PIXELS, font)
-    gimp.pdb.gimp_text_layer_resize(text_layer, text_width, text_height)
-    gimp.pdb.gimp_text_layer_set_justification(text_layer, TEXT_JUSTIFY_CENTER)
-    gimp.pdb.gimp_text_layer_set_color(text_layer, font_color)
-    #gimp.pdb.gimp_image_insert_layer(img, text_layer, None, 1) # Seems to get added automatically so this isn't needed
-
-    # Create outline in layer behind text
-    outline_layer = gimp.Layer(img, "Outline", width, height, RGBA_IMAGE, 100, NORMAL_MODE)
-    gimp.pdb.gimp_image_insert_layer(img, outline_layer, None, 1)
-    
-    # Create path/vectors from text and stroke it on outline layer
-    vectors = gimp.pdb.gimp_vectors_new_from_text_layer(img, text_layer)
-    gimp.pdb.gimp_image_insert_vectors(img, vectors, None, -1)
-    gimp.pdb.gimp_edit_stroke_vectors(outline_layer, vectors)
-    
-    # Run "Drop Shadow" plugin/script
-    gimp.pdb.script_fu_drop_shadow(img, text_layer, 5, 5, 10, (0,0,0), 100, False)
-    return img
-    
+                   
 
 register(
         "create_subtitles",
@@ -104,14 +178,14 @@ register(
             (PF_INT, "font_size_px", "The size of the font (in px)", 48),
             (PF_COLOR, "font_color", "The color of the font", (255, 255, 255)),
             (PF_TOGGLE, "save_png", "Save generated images as PNG files", False),
-            (PF_TOGGLE, "save_xcf", "Save generated images as XCF files", False),
             (PF_TOGGLE, "show_images", "Display the images and don't close them", False),
             (PF_TOGGLE, "add_outline", "Stroke the outline of the text", True),
             (PF_COLOR, "outline_color", "The color of the outline", (0, 0, 0)),
             (PF_TOGGLE, "add_drop_shadow", "Apply a drop shadow", True),
-            (PF_TOGGLE, "generate_one_image_per_record", "Just generate one image file per record", True),
+            #FIXME: re-implement this option (PF_TOGGLE, "generate_one_image_per_record", "Just generate one image file per record", True),
+            (PF_TOGGLE, "generate_one_image_per_record", "Just generate one image file per record", False),
             (PF_FLOAT, "frame_rate", "Frames/second (used when generating time-aligned sequence)", 29.97),
-            (PF_FLOAT, "fade_time_sec", "Fade in/out time (seconds)", 0.300),
+            (PF_FLOAT, "fade_time", "Fade in/out time (seconds)", 0.300),
         ],
         [],
         plugin_main)
